c815a59931226edc9672b0a718263ed4fb79f418
Your Name <you@example.com>
2023-10-13 13:34:11 -0300

    Tarefa e ListaTarefa adicionados.

---
+++ src/Main/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,17 @@
+package Main.OperacoesBasicas;
+
+public class Tarefa {
+
+    //atributo
+
+    private String  descricao;
+
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
